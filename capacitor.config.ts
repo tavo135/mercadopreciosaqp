33bd37507f18e38e5b8b47aca0b63075d30d3cfc
tavo135 <tavo1235@proton.me>
2026-08-10 01:48:58 +0000

    Add Android app project & Capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mercadopreciosaqp.app',
+  appName: 'mercadoaqp',
+  webDir: 'dist'
+};
+
+export default config;
